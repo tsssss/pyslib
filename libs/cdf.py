ab8beb3210f1d718c9a9b0fa9e77e5bc5df9e1ff
Sheng Tian <tianx138@umn.edu>
2022-08-25 23:35:07 -0500

    use both pycdf and cdflib to improve performance

--- libs/cdf.py
+++ libs/cdf.py
@@ -5,7 +5,6 @@ import numpy as np
 from spacepy import pycdf   # https://github.com/spacepy/spacepy/blob/master/spacepy/pycdf/
 from spacepy.pycdf import const
 import cdflib   # https://github.com/MAVENSDC/cdflib
-import libs.epoch as epoch
 
 """
 To achieve what my IDL cdf programs can do (https://github.com/tsssss/slib/tree/master/cdf).
@@ -14,109 +13,180 @@ Now I use pycdf (https://github.com/spacepy/spacepy/tree/master/spacepy/pycdf). 
 The ideal solution is still use the route of cdflib, i.e., to avoid installing the CDF libarary separately.
 """
 
-valid_cdftype = dict([
-    (1, 'CDF_INT1'),
-    (2, 'CDF_INT2'),
-    (4, 'CDF_INT4'),
-    (8, 'CDF_INT8'),
-    (11, 'CDF_UINT1'),
-    (12, 'CDF_UINT2'),
-    (14, 'CDF_UINT4'),
-    (21, 'CDF_REAL4'),
-    (22, 'CDF_REAL8'),
-    (31, 'CDF_EPOCH'),
-    (32, 'CDF_EPOCH16'),
-    (33, 'CDF_TIME_TT2000'),
-    (41, 'CDF_BYTE'),
-    (44, 'CDF_FLOAT'),
-    (45, 'CDF_DOUBLE'),
-    (51, 'CDF_CHAR'),
-    (52, 'CDF_UCHAR'),
-])
+valid_cdf_type = {
+    1:  'CDF_INT1',
+    2:  'CDF_INT2',
+    4:  'CDF_INT4',
+    8:  'CDF_INT8',
+    11: 'CDF_UINT1',
+    12: 'CDF_UINT2',
+    14: 'CDF_UINT4',
+    21: 'CDF_REAL4',
+    22: 'CDF_REAL8',
+    31: 'CDF_EPOCH',
+    32: 'CDF_EPOCH16',
+    33: 'CDF_TIME_TT2000',
+    41: 'CDF_BYTE',
+    44: 'CDF_FLOAT',
+    45: 'CDF_DOUBLE',
+    51: 'CDF_CHAR',
+    52: 'CDF_UCHAR',
+}
+valid_cdf_type_code = list(valid_cdf_type.keys())
+valid_cdf_type_str = list(valid_cdf_type.values())
+
+def get_cdf_type_str(code):
+    return valid_cdf_type_str[valid_cdf_type_code.index(int(code))]
+def get_cdf_type_code(str):
+    return valid_cdf_type_code[valid_cdf_type_str.index(str.upper())]
+
+
+valid_coding = {
+    1:  'NETWORK',
+    2:  'SUN',
+    3:  'VAX',
+    4:  'DECSTATION', 
+    5:  'SGi',
+    6:  'IMBPC',
+    7:  'IBMRS',
+    8:  'HOST',
+    9:  'PPC',
+    11: 'HP',
+    12: 'NeXT',
+    13: 'ALPHAOSF1',
+    14: 'ALPHAVMSd',
+    15: 'ALPHAVMSg',
+    16: 'ALPHAVMSi',
+    17: 'ARM_LITTLE',
+    18: 'ARM_BIG',
+}
+
+valid_checksum = {
+    0: 'NO_CHECKSUM',
+    1: 'MD5_CHECKSUM',
+    2: 'OTHER_CHECKSUM',
+}
+
+valid_scope = {
+    1: 'GLOBAL_SCOPE',
+    2: 'VARIABLE_SCOPE',
+}
+
+valid_majority = {
+    1: 'ROW_MAJORITY',
+    2: 'COLUMN_MAJORITY',
+}
+
+valid_rec_vary = {
+    True: 'VARY',
+    False:'NOVARY',
+}
+
+valid_dim_vary = {
+    -1: 1,  # VARY
+    0:  0,  # NOVARY
+}
+
+valid_cdf_format = {
+    1: 'SINGLE_FILE',
+    2: 'MUTI_FILE',
+}
+
+valid_compression = {
+    0: 'NO_COMPRESSION',
+    1: 'RLE_COMPRESSION',
+    2: 'HUFF_COMPRESSION',
+    3: 'AHUFF_COMPRESSION',
+    5: 'GZIP_COMPRESSION',
+}
 
 
 class cdf():
     filename = None
-    pycdf = None
+    _pycdf = None
+    _cdflib = None
     
     def __init__(self, filename):
         self.filename = filename
         if os.path.exists(filename):
-            self.pycdf = pycdf.CDF(filename, create=False, readonly=False)
+            self._pycdf = pycdf.CDF(filename, create=False, readonly=False)
         else:
-            self.pycdf = pycdf.CDF(filename, create=True)
+            self._pycdf = pycdf.CDF(filename, create=True)
         
         _libpath, _library = pycdf.Library._find_lib()
         self.lib = pycdf.Library(_libpath, _library)
+        self._cdflib = cdflib.CDF(filename)
+        self.cdf_info = self._cdflib.cdf_info()
+
 
     def __del__(self):
-        self.pycdf.close()
+        self._pycdf.close()
 
     def gatts(self):
-        return list(self.pycdf.attrs.keys())
+        return list(self._pycdf.attrs.keys())
 
     def vatts(self):
         _vatts = list()
-        for var in self.vars():
-            vatts = self.read_var_att(var).keys()
-            for vatt in vatts:
-                if vatt not in _vatts: _vatts.append(vatt)
+        atts = self.cdf_info['Attributes']
+        for att in atts:
+            for a in att:
+                if att[a] == 'Variable':
+                    _vatts.append(a)
         return _vatts
+
 
     def read_setting(self, var=None):
         if var is None:
-            return dict(self.pycdf.attrs)
+            return dict(self._pycdf.attrs)
         else:
             return self.read_var_att(var)
 
     def vars(self):
-        return list(self.pycdf.keys())
+        return list(self._pycdf.keys())
 
     def has_var(self, var):
         return var in self.vars()
 
-    def _transpose_data(self, data):
-        dim = data.shape
-        if len(dim) < 2: return data
-        dim_order = np.roll(np.flip(np.arange(len(dim))),1)
-        return np.transpose(data, dim_order)
-
     def read_var(self, var='', range=[], step=1):
         if not self.has_var(var):
             raise Exception(f'{var} is not found ...')
-        # The IDL version has a functionality to shrink unnecessary dimensions.
-        # So far I don't know if this is done already in pycdf.
-        # Thus, let's do the simple thing to return as-is.
-        nrange = len(range)
-        if nrange == 0:
-            data = self.pycdf[var]
-        elif nrange > 2:
-            raise Exception(f'Invalid range: {range} ...')
-        else:
-            data = self.pycdf[var]
-            dim = data.shape
-            nrec = dim[0]
-            _range = range.copy()
-            if nrange == 1: _range.append(_range[0]+1)
 
-            _range.sort()
-            if _range[0] < 0: _range[0] = 0
-            if _range[1] > nrec: _range[1] = nrec
-            if _range[1] < _range[0]:
-                raise Exception(f'Invalid range: {range} ...')
-
-            data = self.pycdf[var][_range[0]:_range[1]:step]
-        data = data[...]
-        
         # pycdf converts time to datetime.
         # This is way too smart... 
         var_info = self.read_var_info(var)
-        data_type = var_info['cdftype']
-        if data_type in ['CDF_EPOCH','CDF_EPOCH16','CDF_TT2000']:
-            data_type = data_type.lower()
-            data = epoch.convert_time(data, input='datetime', output=data_type)
 
-        return data
+        # Work on range.
+        maxrec = var_info['maxrec']
+        nrange = len(range)
+        if len(range) > 2:
+            raise Exception(f'Invalid range: {range} ...')
+        _range = range.copy()
+        if nrange == 1:
+            _range.append(_range[0]+1)
+        elif nrange == 0:
+            _range = [0,maxrec]
+        else:
+            _range.sort()
+        _range = np.clip(_range, 0, maxrec)
+
+
+        # Read data.
+        data_type = var_info['cdf_type']
+        if data_type in ['CDF_EPOCH','CDF_EPOCH16','CDF_TT2000']:
+            data = self._cdflib.varget(var, startrec=_range[0], endrec=_range[1])
+            return data[::step]
+        else:
+            data = self._pycdf[var][_range[0]:_range[1]:step]
+            return data[...]
+
+
+
+        # The IDL version has a functionality to shrink unnecessary dimensions.
+        # So far I don't know if this is done already in pycdf.
+        # Thus, let's do the simple thing to return as-is.
+
+        
+
 
 
 
@@ -124,7 +194,7 @@ class cdf():
     def read_var_att(self, var=''):
         if not self.has_var(var):
             raise Exception(f'{var} is not found ...')
-        return dict(self.pycdf[var].attrs)
+        return dict(self._pycdf[var].attrs)
 
     def read_var_info(self, var=''):
         if not self.has_var(var):
@@ -132,18 +202,19 @@ class cdf():
 
         the_cdf = cdflib.CDF(self.filename)
         var_info = the_cdf.varinq(var)
-        dimvary = var_info['Dim_Vary']
-        dimvary = [int(x != 0) for x in dimvary]
+        dim_vary = var_info['Dim_Vary']
+#        dim_vary = [int(x != 0) for x in dim_vary]
+        dim_vary = [valid_dim_vary[x] for x in dim_vary]
         
         return {
             'name': var_info['Variable'],
-            'cdftype': valid_cdftype[var_info['Data_Type']],
+            'cdf_type': get_cdf_type_str(var_info['Data_Type']),
             'nelem': var_info['Num_Elements'],
             'iszvar': True,
-            'recvary': var_info['Rec_Vary'],
+            'rec_vary': var_info['Rec_Vary'],
             'maxrec': var_info['Last_Rec'],
             'dims': var_info['Dim_Sizes'],
-            'dimvary': dimvary,
+            'dim_vary': dim_vary,
         }
         
 
@@ -151,28 +222,28 @@ class cdf():
     def del_var(self, var=''):
         if not self.has_var(var):
             return
-        del self.pycdf[var]
+        del self._pycdf[var]
 
     def del_setting(self, key, var=None):
         if var is None:
-            self.pycdf.attrs.pop(key, None)
+            self._pycdf.attrs.pop(key, None)
         else:
-            self.pycdf[var].attrs.pop(key, None)
+            self._pycdf[var].attrs.pop(key, None)
 
     def rename_setting(self, key, to='', var=None):
         if var is None:
-            val = self.pycdf.attrs.pop(key, None)
-            if val is not None: self.pycdf.attrs[to] = val
+            val = self._pycdf.attrs.pop(key, None)
+            if val is not None: self._pycdf.attrs[to] = val
         else:
-            val = self.pycdf[var].attrs.pop(key, None)
-            if val is not None: self.pycdf[var].attrs[to] = val
+            val = self._pycdf[var].attrs.pop(key, None)
+            if val is not None: self._pycdf[var].attrs[to] = val
 
     def save_setting(self, var=None, settings=None):
         if var is None:
-            self.pycdf.attrs.update(settings)
+            self._pycdf.attrs.update(settings)
         else:
             if self.has_var(var):
-                self.pycdf[var].attrs.update(settings)
+                self._pycdf[var].attrs.update(settings)
 
     
     def save_data(self, var, value=None, settings=None):
@@ -186,7 +257,7 @@ class cdf():
         if not self.has_var(var):
             raise Exception(f'{var} is not found ...')
         
-        self.pycdf[var][...] = value
+        self._pycdf[var][...] = value
 
         if settings is not None:
             self.save_setting(var, settings=settings)
@@ -230,11 +301,11 @@ class cdf():
 
 
         if len(dimensions) == 0:
-            self.pycdf.new(var, data=value, type=data_type,
+            self._pycdf.new(var, data=value, type=data_type,
             recVary=rec_vary, n_elements=numelem)
         else:
             dim_vary = np.ones(len(dimensions))
-            self.pycdf.new(var, data=value, type=data_type,
+            self._pycdf.new(var, data=value, type=data_type,
             recVary=rec_vary, n_elements=numelem,
             dims=dimensions, dimVarys=dim_vary)
     
@@ -245,7 +316,7 @@ class cdf():
     def rename_var(self, var, to=''):
         if not self.has_var(var):
             raise Exception(f'{var} is not found ...')
-        self.pycdf[var].rename(to)
+        self._pycdf[var].rename(to)
 
     def read_skeleton(self):
         skeleton = dict()
@@ -290,36 +361,15 @@ class cdf():
         self.lib.call(const.CONFIRM_, const.CDF_DECODING_, ctypes.byref(decoding))
         decoding = decoding.value
         
-        valid_coding = dict([
-            (const.NETWORK_ENCODING.value, 'NETWORK'),
-            (const.SUN_ENCODING.value, 'SUN'),
-            (const.VAX_ENCODING.value, 'VAX'),
-            (const.DECSTATION_ENCODING.value, 'DECSTATION'), 
-            (const.SGi_ENCODING.value, 'SGi'),
-            (const.IBMPC_ENCODING.value, 'IMBPC'),
-            (const.IBMRS_ENCODING.value, 'IBMRS'),
-            (const.HOST_ENCODING.value, 'HOST'),
-            (const.PPC_ENCODING.value, 'PPC'),
-            (const.HP_ENCODING.value, 'HP'),
-            (const.NeXT_ENCODING.value, 'NeXT'),
-            (const.ALPHAOSF1_DECODING.value, 'ALPHAOSF1'),
-            (const.ALPHAVMSd_ENCODING.value, 'ALPHAVMSd'),
-            (const.ALPHAVMSg_ENCODING.value, 'ALPHAVMSg'),
-            (const.ALPHAVMSi_ENCODING.value, 'ALPHAVMSi')
-        ])
         encoding_str = valid_coding[encoding]+'_ENCODING'
         decoding_str = valid_coding[decoding]+'_DECODING'
 
 
         # CDF Format.
-        cdfformat = ctypes.c_long(0)
-        self.lib.call(const.GET_, const.CDF_FORMAT_, ctypes.byref(cdfformat))
-        valid_cdfformat = dict([
-            (const.SINGLE_FILE.value, 'SINGLE_FILE'),
-            (const.MULTI_FILE.value, 'MULTI_FILE'),
-        ])
-        cdfformat = cdfformat.value
-        cdfformat_str = valid_cdfformat[cdfformat]
+        cdf_format = ctypes.c_long(0)
+        self.lib.call(const.GET_, const.CDF_FORMAT_, ctypes.byref(cdf_format))
+        cdf_format = cdf_format.value
+        cdf_format_str = valid_cdf_format[cdf_format]
 
         # nzvar.
         nzvar = len(cdf_info['zVariables'])
@@ -336,7 +386,7 @@ class cdf():
         # Header.
         skeleton['header'] = {
             'copyright': copyright,
-            'cdfformat': cdfformat_str, 
+            'cdf_format': cdf_format_str, 
             'decoding': decoding_str,
             'encoding': encoding_str,
             'filename': self.filename,
@@ -384,7 +434,7 @@ class cdf():
         out_str.append(format.format('DATA ENCODING:')+skeleton["header"]["encoding"])
         out_str.append(format.format('DATA DECODING:')+skeleton["header"]["decoding"])
         out_str.append(format.format('MAJORITY:')+skeleton["header"]["majority"])
-        out_str.append(format.format('FORMAT')+skeleton["header"]["cdfformat"])
+        out_str.append(format.format('FORMAT')+skeleton["header"]["cdf_format"])
         out_str.append('!    R.Vars    Z.Vars    G.Atts    V.Atts')
         out_str.append('!    ------    ------    ------    ------')
         out_str.append(f'     {skeleton["header"]["nrvar"]:<10}{skeleton["header"]["nzvar"]:<10}{skeleton["header"]["ngatt"]:<10}{skeleton["header"]["nvatt"]:<10}')
@@ -405,10 +455,9 @@ class cdf():
         vars = self.vars()
         for var, iter in zip(vars, np.arange(len(vars))):
             var_info = skeleton['vars'][var]
-            if var_info['recvary']: recvary = 'VARY'
-            else: recvary = 'NOVARY'
+            rec_vary = valid_rec_vary[var_info['rec_vary']]
             dim_str = ','.join([str(x) for x in var_info['dims']])
-            dimvary_str = ','.join([str(x) for x in var_info['dimvary']])
+            dimvary_str = ','.join([str(x) for x in var_info['dim_vary']])
             if dim_str == '': dim_str = '0'
             if dimvary_str == '': dimvary_str = '0'
 
@@ -416,7 +465,7 @@ class cdf():
             newline()
             out_str.append('!    CDF Type      # Elem    Max Rec     Rec Vary     Dimensions     Dim Vary')
             out_str.append('!    --------      ------    -------     --------     ----------     --------')
-            out_str.append(f'     {var_info["cdftype"]:<14}{var_info["nelem"]:<10}{var_info["maxrec"]:<12}{recvary:<13}{dim_str:<15}{dimvary_str}')
+            out_str.append(f'     {var_info["cdf_type"]:<14}{var_info["nelem"]:<10}{var_info["maxrec"]:<12}{rec_vary:<13}{dim_str:<15}{dimvary_str}')
             newline()
 
             # Variable attribute.
@@ -435,3 +484,40 @@ class cdf():
         else:
             for s in out_str:
                 print(s)
+
+
+# Test shows that both pycdf and cdflib start to use a lot of memory when loading variables.
+# pycdf takes about half the time of cdflib to load several GB of data.
+
+import time
+
+def test_cdf():
+    file = '/Users/shengtian/rbspa_efw_l2_spec_20180927_v02.cdf'
+    file = '/Users/shengtian/rbspa_l1_vb1_20140802_v02.cdf'
+#    with open(file, 'w+b'):
+#        print(f'{file} opened ...')
+
+    cdfid = cdf(file)
+    print(cdfid.vatts())
+    cdfid.print_skeleton()
+
+    test_vars = ['epoch','vb1']
+
+    cdfid = cdf(file)
+    for var in test_vars:
+        tic = time.perf_counter()
+        data = cdfid.read_var(var, step=1000000)
+        toc = time.perf_counter()
+        print(f'{toc-tic:0.4f} seconds for loading {var}')
+        print(data.shape)
+
+    cdfid = cdflib.CDF(file)
+    cdf_info = cdfid.cdf_info()
+    for var in test_vars:
+        tic = time.perf_counter()
+        data = cdfid.varget(var)
+        toc = time.perf_counter()
+        print(f'{toc-tic:0.4f} seconds for loading {var}')
+
+
+test_cdf()
